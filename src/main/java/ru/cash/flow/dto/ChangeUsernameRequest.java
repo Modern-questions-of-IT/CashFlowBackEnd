b7cdd8e4b8a9d25b6d8aa73e8dc8dfb4e12a81b9
Miqael Nazarian <mikanaz1203@gmail.com>
2024-12-08 02:23:17 +0300

    fix change username

--- src/main/java/ru/cash/flow/dto/ChangeUsernameRequest.java
+++ src/main/java/ru/cash/flow/dto/ChangeUsernameRequest.java
@@ -1,10 +1,14 @@
 package ru.cash.flow.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChangeUsernameRequest {
     private String name;
 }
